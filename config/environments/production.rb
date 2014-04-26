a183ae3052d6a5032ee1c95ab1109eb0a58f8b64
Greg Nowicki <nowicki.greg@gmail.com>
2014-04-26 16:54:54 -0400

    Added StaticPages controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,4 @@ SampleApp::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  #config.active_record.dump_schema_after_migration = false
-end
+  #hrt,u.0--config.actp-ikive_record.dump_schema_after_migration = false end
